343724b403b9ba00e5085fc71626fbabd3559d31
App Academy Student <email@email.com>
2022-06-21 16:14:00 -0400

    added onclick to navbar signin button and successfully updates state to openmodal

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 import SessionModalContainer from '../session_modal/session_modal_container';
 
 class NavBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.props.openModal();
+    }
+
 
     render(){
         
@@ -12,7 +21,7 @@ class NavBar extends React.Component{
                 <span className='search-bar-container'>
                     <input type="text" />
                 </span>
-                <SessionModalContainer />
+                <button className='nav-bar-login' onClick={this.handleClick}>Sign in</button> 
                 <span className='nav-bar-cart'>cart</span>
             </div>
         )
--- frontend/components/nav_bar/nav_bar_container.js
+++ frontend/components/nav_bar/nav_bar_container.js
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import { openModal } from '../../actions/modal_actions';
 import NavBar from "./nav_bar";
 
 const mapStateToProps = state => ({
@@ -6,7 +7,7 @@ const mapStateToProps = state => ({
 }); //to display a message of "Sign In" or "Hello, ${name}!"
 
 const mapDispatchToProps = dispatch => ({
-
+    openModal: () => dispatch(openModal())
 });//probably will give it a ui action to dispatch (closing and opening a model being updated through a handle submit)
 
 const NavBarContainer = connect(mapStateToProps, mapDispatchToProps);
