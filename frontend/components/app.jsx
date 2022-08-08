ea7cbbe36eb89d144f164d1cfbe838cbe402f83c
nivan2 <naranivan@gmail.com>
2022-08-08 16:52:40 -0400

    finished container for category page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import SessionModalContainer from './session_modal/session_modal_container';
 import HomePageContainer from './home_page/home_page_container';
+import CategoryPageContainer from './category_page/category_page_container';
 
 const App = () => (
     <>
@@ -11,6 +12,7 @@ const App = () => (
         
         <Switch>
             <Route exact path='/' component={HomePageContainer}/>
+            <Route exact path='/c/:category' component={CategoryPageContainer}/>
         </Switch>
         
         <SessionModalContainer />
---
+++ frontend/components/category_page/category_page_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import { requestProducts } from '../../actions/product_actions';
+import CategoryPage from './category_page';
+
+const mapStateToProps = (state, ownProps) => ({
+    products: state.entities.products,
+    category: ownProps.match.params.category
+});
+
+const mapDispatchToProps = dispatch => ({
+    requestProducts: category => dispatch(requestProducts(category))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryPage);
