8ff4cbdf4cd3ab20902d1f629698717e9814cb65
nivan2 <naranivan@gmail.com>
2022-08-08 14:01:21 -0400

    added model level validation for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-    validates :name, :description, :price, :user_id, presence: true
+    validates :name, :description, :category, :price, :user_id, presence: true
 
     belongs_to :users,
         primary_key: :id,
