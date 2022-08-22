1182547681fb0d5662ee0c2f2cc608ad6cedf586
nivan2 <naranivan@gmail.com>
2022-08-22 08:47:22 -0400

    finished functionality for cart show page and almost done with cart item component functionality

--- frontend/components/cart/cart_container.js
+++ frontend/components/cart/cart_container.js
@@ -28,6 +28,7 @@ const cartCreator = (users, products, cart) => {
 
     Object.values(cart).forEach( cartItem => {
         formedCart.products[cartItem.productId] = products[cartItem.productId];
+        formedCart.products[cartItem.productId].quanitity = cartItem.quantity;
         formedCart.owners[cartItem.productOwnerId] = users[cartItem.productOwnerId];
     });
 
--- frontend/components/cart/cart_item.jsx
+++ frontend/components/cart/cart_item.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 class CartItem extends React.Component{
     constructor(props){
         super(props);
+
         this.handleRemove = this.handleRemove.bind(this);
-        this.changeQuantity = this.changeQuantity.bind(this);
+        this.handleQuantityChange = this.handleQuantityChange.bind(this);
     }
 
     handleRemove(e){
@@ -15,53 +16,83 @@ class CartItem extends React.Component{
         );
     }
 
-    changeQuantity(e){
+    handleQuantityChange(e){
         e.preventDefault();
+        this.props.updateCartItem(
+            this.props.currentUser.id,
+            this.props.cartProduct.id,
+            e.target.value
+        );
     }
 
     render(){
-        const quantities = new Array(15);
+        //Array.from is an array constructor that can take an element arg 
+        // such as just a { length } object, and actually map over it in
+        // this function since it is still being constructed!
+        // also doesnt create a middleman array!
+        const options = () => Array.from({length: 15}, (undefined, i) => ( 
+            <option value={i+1} key={i}>
+                {i+1}
+            </option>    
+        ));
+
         return(
             <>
-                <div className='cart-item-owner-wrapper'>
-                    <span className='cart-item-owner'>
-                        {this.props.productOwner.name}
-                    </span>
-                </div>
-
-                <div className='cart-item-info-wrapper'>
-                    <div className='cart-item-image'>
-
+                {/* <form> */}
+                    <div className='cart-item-owner-wrapper'>
+                        <span className='cart-item-owner'>
+                            {this.props.productOwner.name}
+                        </span>
                     </div>
-                    <div className='cart-item-info'>
-                        <div className='cart-item-name'>
-                            <span>
-                                {this.props.cartProduct.name}
-                            </span>
+
+                    <div className='cart-item-info-wrapper'>
+                        <div className='cart-item-image'>
+
                         </div>
-                        <div>
-                            <button onClick={this.handleRemove}>
-                                Remove from cart
-                            </button>
+                        <div className='cart-item-info'>
+                            <div className='cart-item-name'>
+                                <span>
+                                    {this.props.cartProduct.name}
+                                </span>
+                            </div>
+                            <div>
+                                <button onClick={this.handleRemove}>
+                                    Remove from cart
+                                </button>
+                            </div>
+
                         </div>
-
                     </div>
-                </div>
 
-                <div className='cart-item-numbers-wrapper'>
-                    <div className='cart-item-quantity-wrapper'>
-                        <select name="cart-item-quantity" id="cart-item-quantity" onChange={this.changeQuantity}>
-                            {
-                                quantities.map((_, i) => {
-                                    
-                                })
-                            }
-                        </select>
-                    </div>
-                </div>          
+                    <div className='cart-item-numbers-wrapper'>
+                        <div className='cart-item-quantity-wrapper'>
+                            
+                            <select name="cart-item-quantity-selector" id="cart-item-quantity-selector"
+                                onChange={this.handleQuantityChange}
+                                defaultValue={this.props.cartProduct.quantity}
+                            >
+                                {options()}
+                            </select>
+
+                            {/* <select
+                                defaultValue={this.props.cartProduct.quantity}
+                                onChange={this.props.handleQuantityChange}
+                                options={options()}
+                            /> */}
+                        </div>
+                    </div>          
+                {/* </form> */}
             </>
         )
     }
 };
 
 export default CartItem;
+
+{/* <select name="cart-item-quantity" id="cart-item-quantity" onChange={this.changeQuantity}>
+                            {
+                                quantities.map((_, i) => {
+                                    
+                                })
+                            }
+                        </select> */}
