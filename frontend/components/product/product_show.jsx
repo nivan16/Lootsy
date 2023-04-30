20559ebd2593faf43b71c5b0fa8f9f197c8ec915
nivan2 <naranivan@gmail.com>
2023-04-30 11:16:07 -0400

    added a right arrow icon to the close modal button and styling to it

--- frontend/components/product/product_show.jsx
+++ frontend/components/product/product_show.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faFaceFrown } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faCaretDown, faFaceFrown } from '@fortawesome/free-solid-svg-icons';
 
 class ProductShow extends React.Component {
     constructor(props){
@@ -284,7 +284,7 @@ class ProductShow extends React.Component {
 
                             <div className='product-cart-added-modal-close-button-container'>
                                 <button className='product-cart-added-modal-close-button'>
-                                    Keep shopping ->
+                                    Keep shopping <FontAwesomeIcon icon={faArrowRight} />
                                 </button>
                             </div>
                         </div>
