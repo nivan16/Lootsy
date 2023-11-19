6a68316c197576ab15ad85d17e2e34e06a81444c
nivan2 <naranivan@gmail.com>
2023-11-18 22:19:26 -0500

    made the create review action as well as the object to pass to the reducers

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,1 +1,14 @@
 import * as ReviewUtils from "../utils/review_utils";
+
+const receiveReview = review => ({
+    type: 'RECEIVE_REVIEW',
+    review
+})
+
+export const createReview = review => (
+    ReviewUtils.createReview(review)
+        .then (
+            review => dispatch(receiveReview(review)),
+            err => dispatch(recieveReviewErrors(err.responseJSON))
+        )
+);
