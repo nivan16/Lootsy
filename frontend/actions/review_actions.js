10124bdc6006ab94bbb4e8e0f0a468727e414c9f
nivan2 <naranivan@gmail.com>
2023-11-18 22:23:52 -0500

    changed the action type in receive review to refer to a constant that will be exported and created a review error action

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,8 +1,16 @@
 import * as ReviewUtils from "../utils/review_utils";
 
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
+export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS'
+
 const receiveReview = review => ({
-    type: 'RECEIVE_REVIEW',
+    type: RECEIVE_REVIEW,
     review
+})
+
+const receiveReviewErrors = errors => ({
+    type: RECEIVE_REVIEW_ERRORS,
+    errors
 })
 
 export const createReview = review => (
