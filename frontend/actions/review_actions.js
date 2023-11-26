f0212907720fed0666bdb3988039640009b89f56
nivan2 <naranivan@gmail.com>
2023-11-25 20:54:24 -0500

    added const type remove reviews

--- frontend/actions/review_actions.js
+++ frontend/actions/review_actions.js
@@ -1,7 +1,8 @@
 import * as ReviewUtils from "../utils/review_utils";
 
 export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
-export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS'
+export const REMOVE_REVIEW = 'REMOVE_REVIEW';
+export const RECEIVE_REVIEW_ERRORS = 'RECEIVE_REVIEW_ERRORS';
 
 const receiveReview = review => ({
     type: RECEIVE_REVIEW,
