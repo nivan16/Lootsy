96e7926bed6c9eba62cff98f8f09440a7b6ebe9f
nivan2 <naranivan@gmail.com>
2023-12-02 22:49:38 -0500

    added render of review form as well as the handle change events for review and rating

--- frontend/components/review/review_form.jsx
+++ frontend/components/review/review_form.jsx
@@ -11,9 +11,54 @@ class ReviewForm extends React.Component{
             rating: 0,
             review: "",
         }
+
+        this.handleRatingChange = this.handleRatingChange.bind(this);
+        this.handleReviewChange = this.handleReviewChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleRatingChange(newRating){
+        this.setState({
+            rating: newRating
+        })
+    }
 
+    handleReviewChange(e){
+        e.stopPropagation();
+        this.setState({
+            review: e.target.value    
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+
+    }
+
+    render(){
+        <div className="review-form-container">
+            <form className="review-form" onSubmit={this.handleSubmit}>
+                <h1 className="review-form-header">
+                    Write a review
+                </h1>
+                
+                <div className="review-form-rating-display">
+                    <h2>Rating:</h2>
+                    <StarRatings 
+                        rating={this.state.rating}
+                        changeRating={this.handleRatingChange}
+                        numberOfStars={5}
+                        name="rating"
+                        starDimension="20px"
+                        starSpacing="1px"
+                    />
+
+                </div>
+
+            </form>
+        </div>
+
+    }
 
 
 }
