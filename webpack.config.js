2a368925e75046f708af7ad8c467c4f98ef9c7d9
nivan2 <naranivan@gmail.com>
2024-02-19 05:43:47 -0500

    a rollback of the renaming

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	context: __dirname,
-	entry: './frontend/cutesy.jsx',
+	entry: './frontend/lootsy.jsx',
 	output: {
 		path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
 		filename: 'bundle.js'
