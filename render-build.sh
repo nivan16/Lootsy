bd560aaefa854c32162de4e0de1b1d126de8fda7
nivan2 <naranivan@gmail.com>
2024-02-23 02:31:31 -0500

    moved the render build file into root directory and reverted some cmds to have the bin prefix

--- render-build.sh
+++ render-build.sh
@@ -4,10 +4,10 @@
 set -o errexit
 
 # Precompile assets for production
-RAILS_ENV=production rails assets:precompile
+bin/rails assets:precompile
 
 
 npm run build
 bundle install
-rails db:migrate
-rails db:seed #if needed
+bin/rails db:migrate
+bin/rails db:seed #if needed
